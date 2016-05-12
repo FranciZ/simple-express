e2d606d3457970a57c1455dbd48134415a8971e0
FranciZ <franci@proxima.si>
2016-05-12 17:22:39 +0200

    first commit

---
+++ run.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var server = express();
+
+server.get('/', function(req, res){
+
+    res.send('Hello world!');
+
+});
+
+server.listen(3000, function(){
+
+    console.log('Server running');
+
+});
